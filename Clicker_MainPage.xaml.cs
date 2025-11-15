922eb5bd73a37cd989be1082cf3d3b6d239e1f07
Adrienne Villarena <villarena2707@gmail.com>
2025-11-15 10:45:19 -0600

    uhjiufhadiyhasiudb IM DONE BRO

--- Clicker_MainPage.xaml.cs
+++ Clicker_MainPage.xaml.cs
@@ -1,16 +1,53 @@
+using System.Drawing;
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
 using AudioToolbox;
 
 namespace THEArcadeAppAV;
 
 public partial class Clicker_MainPage : ContentPage
 {
-    
+
     public Bank Bankk = new Bank("Adrienne", 0, 10, 100, 1);
     public Clicker clickerr = new Clicker(1, 5, 1);
     public PassiveIncome PassiveIncomee = new PassiveIncome(0, 100, 1);
-    
+    public Interest interest = new Interest(0, 10000, 1);
+
+
+    public static string[] acheievements = new string[4]
+    {
+        "Acheievement: First Click",
+        "Acheievement: 67",
+        "Acheievement: wow ur rich",
+        "Acheievement: 100G wowowoowow",
+    };
+
+    public static bool[] acheievementsOnce = new bool[4]
+    {
+        false, false, false, false
+    };
+
+    private readonly List<Label> activePopup = new();
+    public async Task Showpopupasync(string text)
+    {
+        var popup = new Label
+        {
+            Text = text,
+            TextColor = Colors.Gold,
+            FontAttributes = FontAttributes.Bold,
+            FontSize = 20,
+            HorizontalOptions = LayoutOptions.Center,
+            Opacity = 1,
+            TranslationY = 0,
+        };
+        objectclicked.Children.Add(popup);
+        activePopup.Add(popup);
+        await popup.TranslateTo(0, -80, 800, Easing.SinInOut);
+        await popup.FadeTo(0, 400);
+        objectclicked.Children.Remove(popup);
+        activePopup.Remove(popup);
+    }   
 
     public class Clicker
     {
@@ -65,28 +102,83 @@ public partial class Clicker_MainPage : ContentPage
                 this.passiveincome_Cost = passiveincome_Cost;
                 this.passiveincome_Level = passiveincome_Level;
             }
-            public void Upgrade()
+        public void Upgrade()
         {
             passiveincome_Value += 3;
             passiveincome_Cost = passiveincome_Cost * 2 - 3;
             passiveincome_Level++;
         }
+        
     }
 
-    
-    
-    public Clicker_MainPage ()
+    public class Interest
+    {
+        public int interest_value;
+        public int interest_cost;
+        public int interest_level;
+        public Interest(int interest_value, int interest_cost, int interest_level)
+        {
+            this.interest_value = interest_value;
+            this.interest_cost = interest_cost;
+            this.interest_level = interest_level;
+        }
+        public void Upgrade()
+        {
+            interest_value += 5;
+            interest_cost = interest_cost * 2 - 250;
+            interest_level++;
+        }
+    }
+
+    public Clicker_MainPage()
     {
         InitializeComponent();
+        animation();
         FirstUpgrade.Text = "clicker upgrade = G" + clickerr.clicker_Cost;
         SecondUpgrade.Text = "passive upgrade: G" + PassiveIncomee.passiveincome_Cost;
-
+        ThirdUpgrade.Text = "interest upgrade: G" + interest.interest_cost;
     }
 
-    private void JunimoTapped(object sender, TappedEventArgs e)
+    public async void acheievementpopup(string message)
+    {
+        var popupacheievement = new Label { Text = message, FontSize = 40, HorizontalOptions = LayoutOptions.Center, TextColor=Colors.Black};
+        acheievementpopuplayout.Children.Add(popupacheievement);
+        await popupacheievement.FadeTo(1, 300);
+        await Task.Delay(5000);
+        await popupacheievement.FadeTo(0, 300);
+        acheievementpopuplayout.Children.Remove(popupacheievement);
+    }
+    
+    public void acheievementcheckfunction()
+    {
+        if (Bankk.balance == 67 && acheievementsOnce[1] == false)
+        {
+            acheievementpopup(acheievements[1]);
+            acheievementsOnce[1] = true;
+        }
+        else if (Bankk.balance >= 100 && acheievementsOnce[3] == false)
+        {
+            acheievementpopup(acheievements[3]);
+            acheievementsOnce[3] = true;
+        }
+        else if (Bankk.balance == 1 && acheievementsOnce[0] == false)
+        {
+            acheievementpopup(acheievements[0]);
+            acheievementsOnce[0] = true;
+        }
+        else if (Bankk.balance >= 10000 && acheievementsOnce[2] == false)
+        {
+            acheievementpopup(acheievements[2]);
+            acheievementsOnce[2] = true;
+        }
+    }
+
+    private async void JunimoTapped(object sender, TappedEventArgs e)
     {
         Bankk.addBalance(clickerr.clicker_Value);
+        acheievementcheckfunction();
         changeMoney_label();
+        await Showpopupasync("+" + clickerr.clicker_Value);
     }
 
     private async void FirstUpgrade_Clicked(object sender, EventArgs e)
@@ -102,7 +194,7 @@ public partial class Clicker_MainPage : ContentPage
         {
             //FirstUpgrade.BackgroundColor = Colors.DarkRed;
             await Task.Delay(1000);
-            //FirstUpgrade.BackgroundColor = Colors.
+            //FirstUpgrade.BackgroundColor = Colors.("#e88c14");
         }
     }
 
@@ -133,9 +225,46 @@ public partial class Clicker_MainPage : ContentPage
     async public void PassiveIncomeee()
     {
         Bankk.addBalance(PassiveIncomee.passiveincome_Value);
+        acheievementcheckfunction();
         changeMoney_label();
+        await Showpopupasync("+" + clickerr.clicker_Value);
         await Task.Delay(1000);
         PassiveIncomeee();
     }
+
+    private async void ThirdUpgrade_Clicked(object sender, EventArgs e)
+    {
+        if (Bankk.balance >= interest.interest_cost)
+        {
+            Bankk.balance -= interest.interest_cost;
+            interest.Upgrade();
+            ThirdUpgrade.Text = "interest upgrade: G" + interest.interest_cost;
+            changeMoney_label();
+        }
+        else
+        {
+            //FirstUpgrade.BackgroundColor = Colors.DarkRed;
+            await Task.Delay(1000);
+            //FirstUpgrade.BackgroundColor = Colors.
+        }
+    }
     
+    public async void animation()
+    {
+        if (firstimage.IsVisible == true)
+        {
+            firstimage.IsVisible = false;
+            secondimage.IsVisible = true;
+            await Task.Delay(1000);
+            animation();
+        }
+        else
+        {
+            firstimage.IsVisible = true;
+            secondimage.IsVisible = false;
+            await Task.Delay(1000);
+            animation();
+        }
+    }
+
 }
