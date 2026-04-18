628dec28390d06246b78afd076b7212735409507
Adrienne Villarena <villarena2707@gmail.com>
2026-04-18 10:54:35 -0500

    coding for zoo is don for today

--- Zoo.xaml.cs
+++ Zoo.xaml.cs
@@ -1,9 +1,20 @@
 using System.Data.Common;
+using System.Runtime.CompilerServices;
+using THEArcadeAppAV.Models;
 
 namespace THEArcadeAppAV;
 
 public partial class Zoo : ContentPage
 {
+	public List<CardBoardSquare> CardBoard = new List<CardBoardSquare>();
+	public List<Card> PlayerDeck = new List<Card>();
+	public List<Card> PlayerHand = new List<Card>();
+	public List<Card> PlayerField = new List<Card>();
+
+	public List<Card> AiDeck = new List<Card>();
+	public List<Card> AiHand = new List<Card>();
+	public List<Card> AiField = new List<Card>();
+	public bool cardIsSelected = false;
 	List<String> commonList = new List<String>()
 	{
 		"a1",
@@ -13,18 +24,23 @@ public partial class Zoo : ContentPage
 		"a5",
 		"a6",
 		"a7",
-		"a8"
+		"a8",
+		"a13"
 	};
 	List<String> rareList = new List<String>()
 	{
-
+		"a13",
+		"a14",
+		"a15",
+		"a16",
+		"a17"
 	};
 	List<String> legendaryList = new List<String>()
 	{
 		"a9",
 		"a10",
-		"a11",
-		"a12"
+		"a11c",
+		"a12c"
 	};
 	public Zoo()
 	{
@@ -56,6 +72,10 @@ public partial class Zoo : ContentPage
 		App.UserRepo.AddCard("a10", "a10.png", 110, 5000, "legendary"); //legendary
 		App.UserRepo.AddCard("a11c", "a11.png", 95, 1500, "legendary"); //Crimson fish
 		App.UserRepo.AddCard("a12c", "a9.png", 85, 900, "legendary"); //Angler
+
+		PopulateDeck(AiDeck);
+		PopulateDeck(PlayerDeck);
+
 		CreateBoard();
 	}
 
@@ -84,8 +104,261 @@ public partial class Zoo : ContentPage
 					BackgroundColor = color
 				};
 
+				if (j == 0) //ai AiHand
+				{
+					sq.Source = AiDeck[i].Name + ".png";
+					Card newCard = new Card(AiDeck[i].Name, i , j);
+					AiHand.Add(newCard);
+					AiDeck.RemoveAt(i);
+				}
+
+				if (j == 3) //player hand
+				{
+					sq.Source = PlayerDeck[i].Name + ".png";
+					Card newCard = new Card(PlayerDeck[i].Name, i , j);
+					PlayerHand.Add(newCard);
+					PlayerDeck.RemoveAt(i);
+				}
+
 				GameBoard.Add(sq , i , j);
+				CardBoard.Add(new CardBoardSquare(this, sq, i ,j));
 			}
 		}
 	}
+
+	public void PopulateDeck(List<Card> Deck)
+	{
+		int common = 15;
+		int rare = 10;
+		int legendary = 5;
+		Random rand = new Random();
+
+		for(int i = 0; i < common; i++)
+		{
+			//rand num genderated
+			int randNum = rand.Next(commonList.Count());
+
+			//card will drawn from the rar list
+			string name = commonList[randNum];
+
+			//a card will be drawn from the darabase
+			Cards result = App.UserRepo.GetCards(name);
+
+			//a new card objet will be Created
+			Card newCard = new Card(result.Name, 0, 0);
+
+			//the new card will be added to the Deck
+			Deck.Add(newCard);
+		}
+
+		for(int i = 0; i < rare; i++)
+		{
+			//rand num genderated
+			int randNum = rand.Next(rareList.Count());
+
+			//card will drawn from the rar list
+			string name = rareList[randNum];
+
+			//a card will be drawn from the darabase
+			Cards result = App.UserRepo.GetCards(name);
+
+			//a new card objet will be Created
+			Card newCard = new Card(result.Name, 0, 0);
+
+			//the new card will be added to the Deck
+			Deck.Add(newCard);
+		}
+
+		for(int i = 0; i < legendary; i++)
+		{
+			//rand num genderated
+			int randNum = rand.Next(legendaryList.Count());
+
+			//card will drawn from the rar list
+			string name = legendaryList[randNum];
+
+			//a card will be drawn from the darabase
+			Cards result = App.UserRepo.GetCards(name);
+
+			//a new card objet will be Created
+			Card newCard = new Card(result.Name, 0, 0);
+
+			//the new card will be added to the Deck
+			Deck.Add(newCard);
+
+			ShuffleDeck(Deck);
+		}
+
+		//shuffle Deck
+		public List<Card> ShuffleDeck(List<Card> deck)
+		{
+			for(int i = deck.Count() - 1; i >= 0; i--)
+			{
+				Random shuffleNum = new Random();
+				int k = shuffleNum.Next(i + 1);
+				var card = deck[k];
+				deck[k] = deck[i];
+				deck[i] = card;
+			}
+			return deck;
+		}
+
+		public Card IdentifyCard(int[] location)
+		{
+			foreach (Card card in PlayerHand)
+			{
+				if (card.currentLocation[0] == location[0] && card.currentLocation[1] == location[1])
+				{
+					return card;
+				}
+			}
+			foreach(Card card in AiHand)
+			{
+				if (card.currentLocation[0] == location[0] && card.currentLocation[1] == location[1])
+				{
+					return card;	
+				}
+			}
+			return null;
+		}
+	}
+
+	public class Card
+	{
+		public string Name;
+		public int[] currentLocation = new int[2];
+		public Card(string name, int i, int j)
+		{
+			this.Name = name;
+			currentLocation[0] = i;
+			currentLocation[1] = j;
+		}
+	}
+
+	public class CardBoardSquare
+	{
+		Zoo p;
+		public ImageButton square;
+		public int[] location = new int[2];
+		public bool isActive = false;
+		public bool chosenForMove = false;
+		public bool chosenforattack = false;
+		public int currentState = 0;
+		public EventHandler DoToggle;
+        public EventHandler DoMove;
+		public EventHandler CanAttack;
+
+		public CardBoardSquare(Zoo page, ImageButton sq, int i, int j)
+		{
+			p = page;
+			square = sq;
+			location[0] = i;
+			location[1] = j;
+			ToggleCard();
+		}
+
+		public void ToggleCard()
+		{
+			if(Convert.ToString(square.Source).Length > 3) //if there is a card
+			{
+				isActive = true;
+				if (IsUserCard())
+				{
+					DoToggle = (sender, args) =>
+					{
+						Card currentCard = p.IdentifyCard(location); //get current card
+						if(currentState == 0 && (p.cardIsSelected == false))
+						{
+							square.Scale = 1.1;
+							currentState = 1;
+							p.cardIsSelected = true;
+							//check for attack or move class
+							CheckMoveOrAttack(currentCard);
+						}
+						else if (chosenforattack || chosenForMove)
+						{
+							chosenForMove = false;
+							chosenforattack = false;
+							currentState = 0;
+							p.cardIsSelected = false;
+							square.Scale = 1;
+						}
+					};
+					square.Clicked += DoToggle;
+				}
+			}
+			else
+			{
+				//there is no card (sw is empty)
+				if(location[1] == 2)
+				{
+					//user move here
+					EmptySquare();
+				}
+				if(location[1] == 2)
+				{
+					//user can attack here
+					EnemySquare();
+				}
+			}
+		}
+	}
+	public bool IsUserCard()
+	{
+		if (location[1] == 2 || location[1] == 3)
+		{
+			return true;
+		}
+		return false;
+	}
+
+	public void CheckMoveOrAttack(Card card)
+	{
+		if (card.currentLocation[1] == 3) // if card is in user hand
+		{
+			chosenForMove = true;
+		}
+		else //else 
+		{
+			chosenforattack = true;
+		}
+	}
+
+	public void EmptySquare()
+	{
+		DoMove = (sender, args) =>
+		{
+			if(Convert.ToString(square.BackgroundColor) == Convert.ToString(Color.FromRgb(255, 255, 255)) || Convert.ToString(square.Source).Length == 0
+			{
+				currentState = 0;
+				chosenForMove = false;
+				p.cardIsSelected = false;
+				square.BackgroundColor = Color.FromRgb(0, 0, 0); //black
+			}
+		}
+		square.Clicked += DoMove;
+	}
+	// color = Color.FromRgb(255, 255, 255); //white
+
+	public void EnemySquare()
+	{
+		CanAttack = (sender, args) =>
+		{
+			if(Convert.ToString(square.Source).Length > 1)
+			{
+				currentState = 0;
+				chosenforattack = false;
+				p.cardIsSelected = false;
+				//player attack
+			}
+		}
+		square.Clicked += CanAttack;
+	}
+
+	public void RemoveEvents()
+	{
+		square.Clicked -= DoToggle;
+		square.Clicked -= DoMove;
+	}
+
 }
